8c2f2a267ea5d2f3991c0cbf7b863b615ffb4667
chuprasov <125488152+chuprasov@users.noreply.github.com>
2023-11-19 23:14:48 +0300

    framework verify email

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -11,9 +11,6 @@
 
     <form id="send-verification" method="post" action="{{ route('verification.send') }}">
         @csrf
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Verify E-mail') }}</x-primary-button>
-        </div>
     </form>
 
     <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
@@ -31,7 +28,7 @@
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail &&  ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
                         {{ __('Your email address is unverified.') }}
